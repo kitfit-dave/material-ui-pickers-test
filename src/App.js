1a282d31d9c9ba252c8e764140c9e178d2374ac0
David Clark <41273710+kitfit-dave@users.noreply.github.com>
2018-11-07 00:04:36 +1100

    keydown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import MomentUtils from 'material-ui-pickers/utils/moment-utils';
-import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
-import LuxonUtils from 'material-ui-pickers/utils/luxon-utils';
-import { MuiPickersUtilsProvider } from 'material-ui-pickers'
-import BasicUsage from './BasicUsage'
+import React from "react";
+import MomentUtils from "material-ui-pickers/utils/moment-utils";
+import DateFnsUtils from "material-ui-pickers/utils/date-fns-utils";
+import LuxonUtils from "material-ui-pickers/utils/luxon-utils";
+import { MuiPickersUtilsProvider } from "material-ui-pickers";
+import BasicUsage from "./BasicUsage";
 
 function App() {
   return (
     <MuiPickersUtilsProvider utils={MomentUtils}>
       <BasicUsage />
     </MuiPickersUtilsProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/BasicUsage.jsx
+++ src/BasicUsage.jsx
@@ -1,17 +1,17 @@
-import React, { Fragment, PureComponent } from 'react'
-import { DatePicker } from 'material-ui-pickers'
+import React, { Fragment, PureComponent } from "react";
+import { DatePicker } from "material-ui-pickers";
 
 export default class BasicUsage extends PureComponent {
   state = {
-    selectedDate: new Date(),
-  }
+    selectedDate: new Date()
+  };
 
-  handleDateChange = (date) => {
-    this.setState({ selectedDate: date })
-  }
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+  };
 
   render() {
-    const { selectedDate } = this.state
+    const { selectedDate } = this.state;
 
     return (
       <Fragment>
@@ -24,7 +24,7 @@ export default class BasicUsage extends PureComponent {
             onChange={this.handleDateChange}
             animateYearScrolling={false}
             minDate={new Date()}
-            onInputChange={e => console.log('Keyboard Input:', e.target.value)}
+            onInputChange={e => console.log("Keyboard Input:", e.target.value)}
           />
         </div>
 
@@ -32,17 +32,67 @@ export default class BasicUsage extends PureComponent {
           <DatePicker
             keyboard
             label="Masked input"
-            format="dd/MM/yyyy"
-            placeholder="10/10/2018"
+            format="DD/MM/YYYY"
+            placeholder="DD/MM/YYYY"
             // handle clearing outside => pass plain array if you are not controlling value outside
-            mask={value => (value ? [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/] : [])}
+            mask={value =>
+              value
+                ? [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/]
+                : []
+            }
             value={selectedDate}
             onChange={this.handleDateChange}
             disableOpenOnEnter
             animateYearScrolling={false}
           />
         </div>
+
+        <div>
+          <DatePicker
+            keyboard
+            label="Can shortcut slashes"
+            format="DD/MM/YYYY"
+            placeholder="DD/MM/YYYY"
+            // handle clearing outside => pass plain array if you are not controlling value outside
+            mask={value =>
+              value
+                ? [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/]
+                : []
+            }
+            value={selectedDate}
+            onChange={this.handleDateChange}
+            disableOpenOnEnter
+            animateYearScrolling={false}
+            onKeyDown={e => {
+              console.log(e.target.selectionStart, e.target.selectionEnd);
+              if (e.key === "/") {
+                let selectionStart = e.target.selectionStart;
+                let [dd, mm, yyyy] = e.target.value.split("/");
+                let set;
+                if (dd && dd.match(/[1-9]_/)) {
+                  console.log("dd");
+                  dd = `0${dd.charAt(0)}`;
+                  selectionStart = 3;
+                  set = true;
+                } else if (mm && mm.match(/[1-9]_/)) {
+                  console.log("mm");
+                  mm = `0${mm.charAt(0)}`;
+                  selectionStart = 6;
+                  set = true;
+                }
+                if (set) {
+                  e.target.value = `${dd}/${mm}/${yyyy}`;
+                  const it = e.target;
+                  setTimeout(
+                    () => it.setSelectionRange(selectionStart, selectionStart),
+                    0
+                  );
+                }
+              }
+            }}
+          />
+        </div>
       </Fragment>
-    )
+    );
   }
 }
